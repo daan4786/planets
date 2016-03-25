71cb9758a7277cb40e442f513be63332928e8d8d
David <daan4786@ufl.edu>
2016-03-25 10:07:07 -0400

    Decided to ignore Pluto, for now

--- planet_list.r
+++ planet_list.r
@@ -1,4 +1,4 @@
 planets = c('mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto')
 print(planets)
 print("But what about Pluto?")
-
+print("Let's ignore pluto for now")
